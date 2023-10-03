489f3943d4ab64a6bb7f827addf4a92a45f9d57b
SeymenOzdes <ozdesxseymen@gmail.com>
2023-10-03 11:32:14 +0300

    Added deleted ContentView

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,43 +6,50 @@
 //
 
 import SwiftUI
-
+struct User: Codable {
+    let firstName: String
+    let lastName: String
+}
 struct ContentView: View {
     @StateObject var expenses = Expenses()
-    @State private var showingAddExpense = false
-    
-    var body: some View {
-        NavigationStack {
-            List {
-                ForEach(expenses.items, id: \.id) { item in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
-                            Text(item.type)
+        @State private var showingAddExpense = false
+
+        var body: some View {
+            NavigationView {
+                List {
+                    ForEach(expenses.items) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+                            }
+
+                            Spacer()
+
+                            Text(item.amount, format: .currency(code: "USD")) 
+                                .foregroundStyle(item.amount > 100 ? Color.primary : Color.secondary)
                         }
-                        Spacer()
-                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                            .foregroundStyle(item.amount > 100 ? Color.primary : Color.secondary)
+                    }
+                    .onDelete(perform: removeItems)
+                }
+                .navigationTitle("iExpense")
+                .toolbar {
+                    Button {
+                        showingAddExpense = true
+                    } label: {
+                        Image(systemName: "plus")
                     }
                 }
-                .onDelete(perform: removeItems)
-            }
-            .toolbar {
-                Button {
-                   showingAddExpense = true 
-                } label: {
-                    Image(systemName: "plus")
+                .sheet(isPresented: $showingAddExpense) {
+                    AddView(expenses: expenses)
                 }
             }
         }
-        .sheet(isPresented: $showingAddExpense) {
-            AddView(expenses: expenses)
+
+        func removeItems(at offsets: IndexSet) {
+            expenses.items.remove(atOffsets: offsets)
         }
-    }
-    func removeItems(at offsets: IndexSet) {
-        expenses.items.remove(atOffsets: offsets)
-    }
 }
 
 #Preview {
